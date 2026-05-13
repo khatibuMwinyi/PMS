af4492b8f033321965639e06df59df13084de38c
Junior-Osborn <juniorkhatib7@gmail.com>
2026-05-13 10:25:01 +0300

    feat: create shared layer (components, hooks, utils, constants)

---
+++ src/shared/index.ts
@@ -0,0 +1,11 @@
+// Shared components
+export * from './components';
+
+// Shared hooks
+export * from './hooks';
+
+// Shared utilities
+export * from './utils';
+
+// Shared constants
+export * from './constants';
